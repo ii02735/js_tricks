f7f13f4c7c3ae5b8f110e3a2b1c471f3cf5cbb47
ik04346 <muhammad.yadallee1@etu.parisdescartes.fr>
2020-01-21 20:52:34 +0100

    init adding of menu

---
+++ script.js
@@ -0,0 +1,24 @@
+window.onload = function() //fonctionne comme ready()
+{	
+
+	switch(page){
+		case "index":
+			document.getElementById("menu").innerHTML = `
+			 <ul> 
+				<li>Accueil</li>
+				<li><a href="articles.html">Articles</a></li>
+			 </ul>`;
+		break;
+
+		case "articles":
+			document.getElementById("menu").innerHTML = `
+			 <ul> 
+				<li><a href="index.html">Accueil</a></li>
+				<li>Articles</li>
+			 </ul>`;
+
+	}
+
+
+
+}
